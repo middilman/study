e7f6f973f607733e9b4377b4b4e751f11dd4ca4c
Middilman <middilman@middil.com>
2013-04-06 10:08:16 -0600

    Car: instance methods

--- car_example/Car.rb
+++ car_example/Car.rb
@@ -2,10 +2,20 @@ class Car
   NUMBER_OF_WHEELS = 4
   # MAKE = 'ford'
   
-
   def initialize(manufacturer, model)
     @manufacturer = manufacturer
     @model = model
   end
-  
+
+  def report_num_wheels
+  	NUMBER_OF_WHEELS
+  end
+
+  def report_model
+  	puts "your model is #{@model}"
+  end
+
+  def report_manufacturer
+  	puts "your manufacturer is #{@manufacturer}"
+  end
 end
