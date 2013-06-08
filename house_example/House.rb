0e1de1c5b1b2eee0c6ba4cc4f7ec0c9f915e0332
Middilman <middilman@middil.com>
2013-06-08 09:41:05 -0600

    attr_accessor

--- house_example/House.rb
+++ house_example/House.rb
@@ -1,7 +1,6 @@
 # create a blueprint for a house object
 class House
-  attr_reader :number_of_people, :number_of_storeys, :roof_color
-  attr_writer :number_of_people, :number_of_storeys, :roof_color
+  attr_accessor :number_of_people, :number_of_storeys, :roof_color
 
   def should_we_turn_off_the_sprinklers
     puts $is_raining
